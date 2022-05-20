88157c94d6fb47ae28b23c6e8bba9c5d8280e614
OlenaLotos <olenameshanych@gmail.com>
2022-05-20 23:47:34 +0300

    Finish HW 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const gallaryContainerRef = document.querySelector('.gallery');
 const gallaryitemsMarkup = createGalleryItemMarkup(galleryItems);
 
 gallaryContainerRef.insertAdjacentHTML('afterbegin', gallaryitemsMarkup);
 gallaryContainerRef.addEventListener('click', onGalleryCardsClick);
+
 // console.log(createGalleryItemMarkup(galleryItems));
+
 function createGalleryItemMarkup(galleryItems) {
   return galleryItems.map(({preview, original, description}) => {
     return `<div class="gallery__item">
@@ -21,22 +24,23 @@ function createGalleryItemMarkup(galleryItems) {
 }) 
 .join('');
 }
- 
+
 function onGalleryCardsClick(event) {
-//  event.preventDefault(); 
- if (!event.target.classList.contains('gallery__image')) { 
- return;}
- else {
-      const instance = basicLightbox.create(`
-       <img
-          src="${event.target.dataset.source}"/>
-      `);
-      instance.show();
-     }
-console.log(event.target);
+  gallaryContainerRef.addEventListener('keydown', (event) => {;
+ if (event.code ==="Escape") {
+  instance.close();
 }
-
-
-
+  });
+ event.preventDefault(); 
+ const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}"/>`
+);
+const galleryItem = event.target.classList.contains("gallery__image");
+if (!galleryItem) {
+  return;
+} else {
+  instance.show();
+}
+}
 
 
